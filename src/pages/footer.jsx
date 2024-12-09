637cf82305f8079c87b9c9289a143515fc697c81
deepakAnkani <deepakdeepu2300@gmail.com>
2024-12-09 11:45:17 +0530

    Durga rao data updated

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -6,20 +6,22 @@ const Footer = () => {
       <div className="copy-right">
         <span>
           E: 
-          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=saikiran.ankani@gmail.com&su=Your Subject Here&body=Your email body here" target="_blank"> saikiran.ankani@gmail.com</a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=saikiran.ankani@gmail.com&su=Your Subject Here&body=Your email body here" 
+          rel="noopener noreferrer"
+          target="_blank"> saikiran.ankani@gmail.com</a>
         </span>
         <span>
-          T: <a href="tel:+917659009114">+91 76590 09114</a>
+          T: <a href="tel:+917659009114">+91 76590 09***</a>
         </span>
       </div>
       <div className="social-media">
         <p className="follow-label">Follow Me</p>
         <div className="social-icon">
-          <a target="_blank" href="https://in.linkedin.com/in/kiran-ankani-98019b178" title="Linkedin">
+          <a target="_blank" rel="noopener noreferrer" href="https://in.linkedin.com/in/kiran-ankani-98019b178" title="Linkedin">
             {" "}
             <span className="fab fa-linkedin-in"></span>{" "}
           </a>
-          <a target="_blank" href="https://github.com/kiranankani" title="Github">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/kiranankani" title="Github">
             {" "}
             <span className="fab fa-github"></span>{" "}
           </a>
